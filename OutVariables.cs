9b89924b1f2f214a15fc5fd75715cd9e5a7c48b9
Keehyuk Lee <keehyuklee@gmail.com>
2018-03-04 22:02:12 -0500

    Add files via upload

---
+++ OutVariables.cs
@@ -0,0 +1,25 @@
+﻿//Out Variable
+
+namespace CSharp7
+{
+    class OutVariables
+    {
+        //Before C#7
+        public double ConvertToDoubleBeforeCS7()
+        {
+            double converted;          
+            double.TryParse("123.45", out converted);
+
+            return converted;
+        }
+
+        //C#7
+        public double ConvertToDoubleCS7()
+        {
+            //C#7 does not need to declair the variable before TryParse method.
+            double.TryParse("123.45", out double converted);
+
+            return converted;
+        }
+    }
+}
